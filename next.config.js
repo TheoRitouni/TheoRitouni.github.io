9ec70cd42baf4eb7ac2e575c63a8eb4874183995
TheoRit <t.ritouni@gmail.com>
2023-12-14 08:29:16 +0100

    [Fix] try some fix version

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
-
 module.exports = {
     experimental: {
       forceSwcTransforms: true,
     },
-    output: 'export',
   }
-
-
-   
