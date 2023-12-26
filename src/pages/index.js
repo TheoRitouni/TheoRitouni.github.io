7c8a7b83cadc10dd80349d6a4e5fbb9dded07e98
TheoRit <t.ritouni@gmail.com>
2023-12-26 17:52:31 +0100

    delete animation background, try to fix bwat image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
+
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
@@ -17,7 +17,6 @@ const Home = () => {
     <Layout>
        <Section grid> 
         <Hero />
-         <BgAnimation /> 
        </Section> 
       <Projects />
       <Technologies />
